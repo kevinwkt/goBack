e6ddf2fc591e5bf623df40e1126d578026ccea52
Gerry <gerrygoo@gmail.com>
2017-02-16 17:12:43 -0600

    make main menu class

---
+++ core/src/com/tec/goback/MainMenu.java
@@ -0,0 +1,49 @@
+package com.tec.goback;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by gerry on 2/16/17.
+ */
+public class MainMenu implements Screen {
+
+    private final App app;
+    public MainMenu(App app) {
+        this.app = app;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
