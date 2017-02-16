bdee74f102f04272142366f8fbb87a65d8b6bc3e
sergiohernandezjr <A01371446@itesm.mx>
2017-02-16 16:59:46 -0600

    Hola Gerry

--- core/src/com/tec/goback/App.java
+++ core/src/com/tec/goback/App.java
@@ -1,12 +1,14 @@
 package com.tec.goback;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class App extends ApplicationAdapter {
+public class App extends Game {
+	//Gerry, eres estupido???
 	SpriteBatch batch;
 	Texture img;
 	
