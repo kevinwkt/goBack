159e5b49c95c403cef1d88fa94a7045b45cf54a7
Pablo Sanchez <pabloastadeo@gmail.com>
2017-03-18 21:57:39 -0600

    estados

---
+++ core/src/com/tec/goback/LoaderState.java
@@ -0,0 +1,21 @@
+package com.tec.goback;
+
+/**
+ * Created by pablo on 18/03/17.
+ */
+
+public enum LoaderState {
+    ABOUT,
+    MAINMENU,
+    PAUSE,
+    SOUNDSETTINGS,
+    STORY,
+    LEVEL1,
+    LEVEL2,
+    LEVEL3,
+    BOSS1,
+    BOSS2,
+    BOSS3,
+    ARCADE
+
+}
