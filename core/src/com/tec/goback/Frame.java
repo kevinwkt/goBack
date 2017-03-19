687d6594e3d1cd30eacb9212563127eb8f75c3e8
sergiohernandezjr <A01371446@itesm.mx>
2017-03-19 15:43:30 -0600

    Frame Class complete

--- core/src/com/tec/goback/Frame.java
+++ core/src/com/tec/goback/Frame.java
@@ -3,6 +3,7 @@ package com.tec.goback;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -25,6 +26,7 @@ public class Frame implements Screen {
     //Main app class
     private final App app;
 
+
     Preferences pref=Gdx.app.getPreferences("getLevel");
 
     //Screen sizes
@@ -37,25 +39,37 @@ public class Frame implements Screen {
     private OrthographicCamera camera;
 
     //Main app class
-    public Viewport view;
+    protected Viewport view;
 
     //Textures
-    private Texture pauseButton;//Image that holds creators photos and back button
+    protected Texture pauseButton;//Image that holds creators photos and back button
 
-    public SpriteBatch batch;
+    protected SpriteBatch batch;
 
     //Stage
-    public Stage frameStage;
+    protected Stage frameStage;
+
+    //Auxiliary screens
+    protected Pause pauseStage;
+
+    //Gamestate
+    protected GameState state = GameState.PLAYING;
+
+    //AssetMng
+    protected AssetManager aManager;
 
     public Frame(App app) {
         this.app = app;
+        aManager= app.getAssetManager();
+
+        pauseStage = new Pause(view, batch, app);
     }
 
     @Override
     public void show() {
         cameraInit();
         textureInit();
-        objectInit();
+        pauseInit();
     }
 
     private void cameraInit() {
@@ -67,15 +81,11 @@ public class Frame implements Screen {
     }
 
     private void textureInit() {
-        pauseButton = new Texture("Interfaces/GAMEPLAY/CONSTANT/GobackCONSTPause.png");
-        backgroundCharactersInit();
+        //Override always
     }
 
-    private void backgroundCharactersInit() {
-        //Override this method always
-    }
-
-    private void objectInit() {
+    private void pauseInit() {
+        pauseButton = aManager.get("Interfaces/GAMEPLAY/CONSTANT/GobackCONSTPause.png");
         batch = new SpriteBatch();
         frameStage = new Stage(view, batch);
 
@@ -120,12 +130,227 @@ public class Frame implements Screen {
 
     @Override
     public void render(float delta) {
-        cls();
-        frameStage.draw();
     }
-    private void cls() {
-        Gdx.gl.glClearColor(0,0,0,1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+    public class Pause extends Stage {
+        private AssetManager aManager;
+        private final App app;
+
+        //Screen sizes
+        public static final float WIDTH = 1280;
+        public static final float HEIGHT = 720;
+        public static final float HALFW = WIDTH/2;
+        public static final float HALFH = HEIGHT/2;
+
+        //Camera
+        private OrthographicCamera camera;
+
+
+        //Viewport
+        private Viewport view;
+
+        //Textures
+        private Texture background; //Background
+        private Texture bottom; //Image
+        private Texture map; //Image
+        private Texture quitBton; //Button
+        private Texture top; //Image
+        private Texture backBton; //Button
+        private Texture musicBton;
+        private Texture fxBton;
+
+        Preferences prefes = Gdx.app.getPreferences("My Preferences");
+
+
+        // buttons
+        private ImageButton musicImg;
+        private TextureRegionDrawable musicPlay;
+        private ImageButton fxImg;
+        private TextureRegionDrawable fxPlay;
+
+        public void changeSoundTexture(){
+            musicImg.remove();
+            if(prefes.getBoolean("soundOn")){
+                musicBton = aManager.get("Interfaces/SOUND/SOUNDMusicON.png");
+            }else{
+                musicBton = aManager.get("Interfaces/SOUND/SOUNDMusic.png");
+            }
+            musicPlay = new TextureRegionDrawable(new TextureRegion(musicBton));
+            musicImg = new ImageButton(musicPlay );
+            musicImg.setPosition(4*WIDTH/7, 8*HEIGHT/15);
+            this.addActor(musicImg);
+
+            musicImg.addListener(new ClickListener(){
+
+                @Override
+                public void clicked(InputEvent event, float x, float y) {
+                    if(prefes.getBoolean("soundOn")){
+                        prefes.putBoolean("soundOn",false);
+                    }else{
+                        prefes.putBoolean("soundOn",true);
+                    }
+                    prefes.flush();
+                    changeSoundTexture();
+                }
+            });
+        }
+
+        private void changeFxTexture() {
+            fxImg.remove();
+            if(prefes.getBoolean("fxOn")){
+                fxBton = aManager.get("Interfaces/SOUND/SOUNDSoundON.png");
+            }else{
+                fxBton = aManager.get("Interfaces/SOUND/SOUNDSound.png");
+            }
+
+            fxPlay = new TextureRegionDrawable(new TextureRegion(fxBton));
+            fxImg = new ImageButton(fxPlay);
+            fxImg.setPosition(4*WIDTH/7, 4*HEIGHT/15);
+            this.addActor(fxImg);
+
+            fxImg.addListener(new ClickListener(){
+
+                @Override
+                public void clicked(InputEvent event, float x, float y) {
+                    if(prefes.getBoolean("fxOn")){
+                        prefes.putBoolean("fxOn",false);
+
+                    }else{
+                        prefes.putBoolean("fxOn",true);
+
+                    }
+                    prefes.flush();
+                    changeFxTexture();
+                }
+            });
+
+        }
+
+        private void textureInit(){
+            background = aManager.get("HARBOR/GoBackHARBOR0.png");
+            bottom = aManager.get("Interfaces/PAUSE/PAUSEBottomDisplay.png");
+            map = aManager.get("Interfaces/PAUSE/PAUSEMapList.png");
+            quitBton = aManager.get("Interfaces/PAUSE/PAUSEQuit.png");
+            top = aManager.get("Interfaces/PAUSE/PAUSETopDisplay.png");
+            backBton = aManager.get("Interfaces/PAUSE/PAUSEback.png");
+
+            if(prefes.getBoolean("soundOn")){
+                musicBton = aManager.get("Interfaces/SOUND/SOUNDMusicON.png");
+            }else{
+                musicBton = aManager.get("Interfaces/SOUND/SOUNDMusic.png");
+            }
+            if(prefes.getBoolean("fxOn")){
+                fxBton = aManager.get("Interfaces/SOUND/SOUNDSoundON.png");
+            }else{
+                fxBton = aManager.get("Interfaces/SOUND/SOUNDSound.png");
+            }
+        }
+
+        private void objectInit(){
+            //background hace Image
+            Image backgroundImg = new Image(background);
+            backgroundImg.setPosition(HALFW-backgroundImg.getWidth()/2, HALFH-backgroundImg.getHeight()/2);
+            this.addActor(backgroundImg);
+
+            //top
+            Image topImg = new Image(top);
+            topImg.setPosition(HALFW-backgroundImg.getWidth()/2, HALFH-backgroundImg.getHeight()/2);
+            this.addActor(topImg);
+
+
+            //bottom
+            //1280x720
+            Image bottomImg=new Image(bottom);
+            bottomImg.setPosition(HALFW-backgroundImg.getWidth()/2, HALFH-backgroundImg.getHeight()/2);
+            this.addActor(bottomImg);
+
+
+            //map
+            Image mapImg = new Image(map);
+            mapImg.setPosition(HALFW-backgroundImg.getWidth()/2, HALFH-backgroundImg.getHeight()/2+220);
+            this.addActor(mapImg);
+
+
+            //quitBton
+            TextureRegionDrawable quitBtonTrd = new TextureRegionDrawable(new TextureRegion(quitBton));
+            ImageButton quitBtonImg = new ImageButton(quitBtonTrd);
+
+            quitBtonImg.setPosition(HALFW-quitBtonImg.getWidth()/2-470, HEIGHT-quitBtonImg.getHeight()/2-640);
+            this.addActor(quitBtonImg);
+
+            quitBtonImg.addListener(new ClickListener(){
+                @Override
+                public void clicked(InputEvent event, float x, float y) {
+                    app.setScreen(new Fade(app, LoaderState.MAINMENU));
+                }
+            });
+
+
+            //back
+            TextureRegionDrawable backBtonTrd = new TextureRegionDrawable(new TextureRegion(backBton));
+            ImageButton backBtonImg = new ImageButton(backBtonTrd);
+
+            backBtonImg.setPosition(HALFW-backBtonImg.getWidth()/2+500, HEIGHT-backBtonImg.getHeight()/2-640);
+            this.addActor(backBtonImg);
+
+            backBtonImg.addListener(new ClickListener(){
+                @Override
+                public void clicked(InputEvent event, float x, float y) {
+                    state = GameState.PLAYING;
+                }
+            });
+
+            //music
+
+            musicPlay = new TextureRegionDrawable(new TextureRegion(musicBton));
+            musicImg = new ImageButton(musicPlay );
+            musicImg .setPosition(4*WIDTH/7, 8*HEIGHT/15);
+            this.addActor(musicImg);
+
+            musicImg.addListener(new ClickListener(){
+
+                @Override
+                public void clicked(InputEvent event, float x, float y) {
+                    if(prefes.getBoolean("soundOn")){
+                        prefes.putBoolean("soundOn",false);
+
+                    }else{
+                        prefes.putBoolean("soundOn",true);
+                    }
+                    prefes.flush();
+                    changeSoundTexture();
+                }
+            });
+
+            //fx
+            fxPlay = new TextureRegionDrawable(new TextureRegion(fxBton));
+            fxImg = new ImageButton(fxPlay);
+            fxImg.setPosition(4*WIDTH/7, 4*HEIGHT/15);
+            this.addActor(fxImg);
+
+            fxImg.addListener(new ClickListener(){
+
+                @Override
+                public void clicked(InputEvent event, float x, float y) {
+                    if(prefes.getBoolean("fxOn")){
+                        prefes.putBoolean("fxOn",false);
+                    }else{
+                        prefes.putBoolean("fxOn",true);
+                    }
+                    prefes.flush();
+                    changeFxTexture();
+                }
+            });
+        }
+
+        public Pause(Viewport view, SpriteBatch batch, App app) {
+            super(view, batch);
+            this.app = app;
+            this.aManager = app.getAssetManager();
+
+            textureInit();
+            objectInit();
+        }
     }
 
     @Override
@@ -135,21 +360,23 @@ public class Frame implements Screen {
 
     @Override
     public void pause() {
-
     }
 
     @Override
     public void resume() {
-
     }
 
     @Override
     public void hide() {
-
     }
 
     @Override
     public void dispose() {
-
     }
+}
+
+enum GameState {
+    PLAYING,
+    LOST,
+    PAUSED
 }
