a430dabc8459b748ef3e97eb4bff6ff356a5c24a
kaero <kevinwkt97@outlook.com>
2017-04-21 18:34:56 -0500

    fuck gerardo

--- core/src/com/tec/goback/ArcadeGoo.java
+++ core/src/com/tec/goback/ArcadeGoo.java
@@ -1,37 +1,40 @@
 package com.tec.goback;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.CircleShape;
-import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 
-/*
+/**
+ * Created by kevin on 4/20/2017.
+ **/
+
 class ArcadeGoo extends Enemy {
-
     private PolygonShape shape;
-    private static float SPEED = 0.4f;
-    private Body body;
+    private int walkCounter;
+    private int walkLimit;
+    private boolean walkCond=true;
 
-    public ArcadeGoo(World world, int type, float angle, Texture tx, float startX, float startY) {
-        super(world,type,angle,tx,startX,startY);
+    public ArcadeGoo(World world, int type, float spawnx,float spawny, Animation tx) {
+        super(world,type,spawnx,spawny,tx);
+        SPEED=0.5f;
+        //VELOCITIES
+        if(x>0) body.setLinearVelocity(-MathUtils.cos(angle) * SPEED, -MathUtils.sin(angle) * SPEED);
+        else body.setLinearVelocity(MathUtils.cos(angle) * SPEED, -MathUtils.sin(angle) * SPEED);
+        timeframe=0;
+        walkCounter=0;
+        dmg=15f;
+        hp=80;
+
     }
 
     void fixturer(float density, float restitution) {
-
-        //neumann preventive shit
-        for (Fixture fix : body.getFixtureList()) {body.destroyFixture(fix);}
-
         //lizard
         shape = new PolygonShape();
-        shape.setAsBox(ArcadeValues.pxToMeters(sprite.getHeight()), ArcadeValues.pxToMeters(sprite.getWidth()));
+        shape.setAsBox(ArcadeValues.pxToMeters(x), ArcadeValues.pxToMeters(y));
 
         FixtureDef fixtureDef = new FixtureDef();
         fixtureDef.density = density;
@@ -39,17 +42,16 @@ class ArcadeGoo extends Enemy {
         fixtureDef.shape = shape;
         fixtureDef.friction = 0;
 
-        fixtureDef.filter.categoryBits = ArcadeValues.pelletCat; //its category
-        fixtureDef.filter.maskBits = ArcadeValues.pelletMask; //or of its category with colliding categories
+        fixtureDef.filter.categoryBits = ArcadeValues.enemyCat; //its category
+        fixtureDef.filter.maskBits = ArcadeValues.enemyMask; //or of its category with colliding categories
 
         body.createFixture(fixtureDef);
     }
 
-    void draw(SpriteBatch batch,float delta) {
-        delta +=Gdx.graphics.getDeltaTime();
-        region=an.getKeyFrame(delta);
+    void draw(SpriteBatch batch) {
+        timeframe += Gdx.graphics.getDeltaTime();
+        region=an.getKeyFrame(timeframe);
         batch.draw(region, ArcadeValues.metersToPx(body.getPosition().x), ArcadeValues.metersToPx(body.getPosition().y));
+        walkCounter++;
     }
 }
-
-*/
---
+++ core/src/com/tec/goback/ArcadeSkull.java
@@ -0,0 +1,8 @@
+package com.tec.goback;
+
+/**
+ * Created by kevin on 4/20/2017.
+ */
+
+public class ArcadeSkull {
+}
