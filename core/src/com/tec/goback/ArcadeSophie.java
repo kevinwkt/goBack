f56f73308edd7888f9b3ed0a69bfba58af7a928e
Pablo Sanchez <pabloastadeo@gmail.com>
2017-04-28 07:56:27 -0500

    Sophie and level 2

--- core/src/com/tec/goback/ArcadeSophie.java
+++ core/src/com/tec/goback/ArcadeSophie.java
@@ -1,8 +1,11 @@
 package com.tec.goback;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
@@ -17,17 +20,68 @@ class ArcadeSophie {
     private Sprite sprite;
     private float life = 100;
     private int color = 1;
+    private Animation<TextureRegion> standby;
+    private Animation<TextureRegion> walking;
+    private Animation<TextureRegion> waking;
+    private Animation<TextureRegion> dying;
+
+    private float timerchangeframestandby;
+    private float timerchangeframewalk;
+    private float timerchangeframedie;
+    private float timerchangeframewake;
+    private Sophie.MovementState currentstate = Sophie.MovementState.STILL_RIGHT; //STILL_RIGHT
+
 
     ArcadeSophie(World world, Texture tx){
-        this.sprite = new Sprite(tx);
-        sprite.setCenter(ArcadeValues.pelletOriginX-100, ArcadeValues.pelletOriginY);
+        TextureRegion texturaCompleta = new TextureRegion(tx);
+
+        TextureRegion[][] texturaPersonaje = texturaCompleta.split(160,160);
+
+        standby = new Animation(0.18f, texturaPersonaje[0][0], texturaPersonaje[0][1],
+                texturaPersonaje[0][2], texturaPersonaje[0][3],texturaPersonaje[0][4],
+                texturaPersonaje[0][5], texturaPersonaje[0][6]);
+        walking = new Animation(0.06f, texturaPersonaje[0][7], texturaPersonaje[0][8],
+                texturaPersonaje[0][9], texturaPersonaje[0][10],
+                texturaPersonaje[0][11], texturaPersonaje[0][12],texturaPersonaje[0][13],
+                texturaPersonaje[0][14], texturaPersonaje[0][15], texturaPersonaje[0][16],
+                texturaPersonaje[0][17], texturaPersonaje[0][18], texturaPersonaje[0][19],
+                texturaPersonaje[0][20],texturaPersonaje[0][21], texturaPersonaje[0][22],
+                texturaPersonaje[0][23], texturaPersonaje[0][24],texturaPersonaje[0][25]);
+        waking= new Animation(0.15f, texturaPersonaje[0][42], texturaPersonaje[0][41],
+                texturaPersonaje[0][40], texturaPersonaje[0][39],
+                texturaPersonaje[0][38], texturaPersonaje[0][37],texturaPersonaje[0][36],
+                texturaPersonaje[0][35], texturaPersonaje[0][34], texturaPersonaje[0][33],
+                texturaPersonaje[0][32], texturaPersonaje[0][31], texturaPersonaje[0][30],
+                texturaPersonaje[0][29],texturaPersonaje[0][28], texturaPersonaje[0][27],
+                texturaPersonaje[0][26]);
+        dying= new Animation(0.1f, texturaPersonaje[0][26], texturaPersonaje[0][27],
+                texturaPersonaje[0][28], texturaPersonaje[0][29],
+                texturaPersonaje[0][30], texturaPersonaje[0][31],texturaPersonaje[0][32],
+                texturaPersonaje[0][33], texturaPersonaje[0][34], texturaPersonaje[0][35],
+                texturaPersonaje[0][36], texturaPersonaje[0][37], texturaPersonaje[0][38],
+                texturaPersonaje[0][39],texturaPersonaje[0][40], texturaPersonaje[0][41],
+                texturaPersonaje[0][42]);
+        // Animación infinita
+        standby.setPlayMode(Animation.PlayMode.LOOP);
+        walking.setPlayMode(Animation.PlayMode.LOOP);
+
+        // Inicia el timer que contará tiempo para saber qué frame se dibuja
+        timerchangeframewalk = 0;
+        timerchangeframestandby=0;
+        timerchangeframedie=0;
+        timerchangeframewake=0;
+
+        sprite = new Sprite(texturaPersonaje[0][0]);
+
+        sprite.setCenter(ArcadeValues.pelletOriginX-100,ArcadeValues.pelletOriginY);
 
         BodyDef bodyDef = new BodyDef();
-        bodyDef.type = BodyDef.BodyType.StaticBody;
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
         bodyDef.position.set(
-                ArcadeValues.meterspelletOriginX-1,
-                ArcadeValues.meterspelletOriginY+(ArcadeValues.pxToMeters(tx.getHeight()/3))
+                ArcadeValues.meterspelletOriginX-7,
+                ArcadeValues.meterspelletOriginY+(ArcadeValues.pxToMeters(tx.getHeight()/4))
         );
+
         body = world.createBody(bodyDef);
         fixturer(0.1f, 0.7f);
         body.setLinearVelocity(0f, 0f);
@@ -35,10 +89,6 @@ class ArcadeSophie {
     }
 
     private void fixturer(float density, float restitution) {
-//        //neumann preventive shit
-//        for (Fixture fix : body.getFixt ureList()) {
-//            body.destroyFixture(fix);
-//        }
 
         //shape of girl
         PolygonShape shape = new PolygonShape();
@@ -53,7 +103,7 @@ class ArcadeSophie {
         fixtureDef.shape = shape;
         fixtureDef.friction = 0;
 
-        fixtureDef.filter.categoryBits = 0; //its category
+        fixtureDef.filter.categoryBits = ArcadeValues.sophieCat; //its category
         fixtureDef.filter.maskBits = ArcadeValues.sophieMask; //or of its category with colliding categories
 
         body.createFixture(fixtureDef);
@@ -69,17 +119,79 @@ class ArcadeSophie {
     }
 
     void draw(SpriteBatch batch) {
+        TextureRegion region;
+        switch (currentstate) {
+            case MOVE_RIGHT:
+            case MOVE_LEFT:
+                timerchangeframewalk += Gdx.graphics.getDeltaTime();
+                region = walking.getKeyFrame(timerchangeframewalk);
+                /*if (currentstate== Sophie.MovementState.MOVE_LEFT) {
+                    if (!region.isFlipX()) {
+                        region.flip(true,false);
+                    }
+                } else {
+                    if (region.isFlipX()) {
+                        region.flip(true,false);
+                    }
+                }*/
+                body.setLinearVelocity(1.5f, 0f);
+                sprite.setCenter(
+                        ArcadeValues.metersToPx(body.getPosition().x),
+                        ArcadeValues.metersToPx(body.getPosition().y)
+                );
+                batch.draw(region,sprite.getX(),sprite.getY());
+                break;
+            case STILL_LEFT:
+            case STILL_RIGHT:
+                body.setLinearVelocity(0f, 0f);
+                timerchangeframestandby += Gdx.graphics.getDeltaTime();
+                region = standby.getKeyFrame(timerchangeframestandby);
+                sprite.setCenter(
+                        ArcadeValues.metersToPx(body.getPosition().x),
+                        ArcadeValues.metersToPx(body.getPosition().y)
+                );
+                batch.draw(region,sprite.getX(),sprite.getY()); // Dibuja el sprite estático
+                break;
+        }
 
-        sprite.setCenter(
-                ArcadeValues.metersToPx(body.getPosition().x),
-                ArcadeValues.metersToPx(body.getPosition().y)
-        );
-        sprite.setColor(1.0f, 1.0f, 1.0f, life/100f);
 
-        sprite.draw(batch);
+
     }
 
     void setColor(int color){
         this.color = color;
+    }
+
+    public Sophie.MovementState getMovementState() {
+        return currentstate;
+    }
+
+    // Modificador de estadoMovimiento
+    public void setMovementState(Sophie.MovementState ms) {
+        this.currentstate = ms;
+    }
+
+    public float getX(){
+        return sprite.getX();
+    }
+
+    public float getY(){
+        return sprite.getY();
+    }
+
+    public Sprite getSprite(){
+        return sprite;
+    }
+
+    public enum MovementState {
+        CREATING,
+        WAKING,
+        STILL_RIGHT,
+        STILL_LEFT,
+        HIT,
+        MOVE_LEFT,
+        MOVE_RIGHT,
+        DYING,
+        SLEEPING
     }
 }
--- core/src/com/tec/goback/Level2.java
+++ core/src/com/tec/goback/Level2.java
@@ -1,22 +1,158 @@
 package com.tec.goback;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Manifold;
+import com.badlogic.gdx.physics.box2d.World;
+
+import java.util.HashSet;
 
 /**
  * Created by sergiohernandezjr on 18/03/17.
  */
 class Level2 extends Frame {
+    //box2d shit
+    private World world;
+    private HashSet<Body> deadThings;
+
+
+    // Sophie
+    //private Sophie sophie;
+    private Texture sophieTexture;
+    //boolean sophieInitFlag = true;
+
+    private ArcadeSophie sophie;
+
+    // background
+    Texture background;
+
+    //map
+    public static final float WIDTH_MAP = 3840;
+    public static final float HEIGHT_MAP = 720;
+
+    // preferences
+    Preferences pref = Gdx.app.getPreferences("getLevel");
+    Preferences soundPreferences = Gdx.app.getPreferences("My Preferences");
+
+
+
     public Level2(App app) {
-        super(app, 3840,720);
+        super(app, WIDTH_MAP,HEIGHT_MAP);
+
+    }
+
+    @Override
+    public void show(){
+        super.show();
+        textureInit();
+        worldInit();
+
+        Gdx.input.setCatchBackKey(true);
+        Gdx.input.setInputProcessor(new Input());
+
+
+
+        
+    }
+
+    private void worldInit() {
+        world = new World(Vector2.Zero, true);
+        deadThings = new HashSet<Body>();
+        world.setContactListener(new ContactListener() {
+             @Override
+             public void beginContact(Contact contact) {
+
+             }
+
+             @Override
+             public void endContact(Contact contact) {
+
+             }
+
+             @Override
+             public void preSolve(Contact contact, Manifold oldManifold) {
+
+             }
+
+             @Override
+             public void postSolve(Contact contact, ContactImpulse impulse) {
+
+             }
+            }
+
+        );
+
+        sophie = new ArcadeSophie(world,sophieTexture);
+    }
+
+    private void textureInit() {
+        // background
+        background = aManager.get("MOUNTAINS/GoBackMOUNTAINSPanoramic.png");
+
+        // sophie
+        sophieTexture = new Texture("Squirts/Sophie/SOPHIEWalk.png");
+
+        
     }
 
     @Override
     public void render(float delta) {
+        cls();
 
+        batch.setProjectionMatrix(super.camera.combined);
+        batch.begin();
+
+        batch.draw(background,0,0);
+        updateCamera();
+        sophieInitialMove();
+        sophie.draw(batch);
+        stepper(delta);
+        batch.end();
+    }
+
+
+
+    private void stepper(float delta){
+        world.step(1/60f, 6, 2);
+
+        for(Body b: deadThings){
+            while(b.getFixtureList().size > 0){
+                b.destroyFixture(b.getFixtureList().get(0));
+            }
+            world.destroyBody(b);
+        }
+        deadThings.clear();
+
+    }
+
+    private void sophieInitialMove() {
+
+        if(sophie.getX() < 250){
+            sophie.setMovementState(Sophie.MovementState.MOVE_RIGHT);
+        }else{
+            sophie.setMovementState(Sophie.MovementState.STILL_RIGHT);
+        }
+
+    }
+
+    private void cls() {
+        Gdx.gl.glClearColor(0,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
     }
 
     @Override
     public void resize(int width, int height) {
+        view.update(width, height);
 
     }
 
@@ -38,5 +174,88 @@ class Level2 extends Frame {
     @Override
     public void dispose() {
 
+    }
+
+    public void updateCamera(){
+        float posX = sophie.getX()+sophie.getSprite().getWidth()/2;
+        // Si está en la parte 'media'
+        if (posX>=HALFW && posX<=WIDTH_MAP-HALFW) {
+            // El personaje define el centro de la cámara
+            camera.position.set((int)posX, camera.position.y, 0);
+        } else if (posX>WIDTH_MAP-HALFW) {    // Si está en la última mitad
+            // La cámara se queda a media pantalla antes del fin del mundo  :)
+            camera.position.set(WIDTH_MAP-HALFW, camera.position.y, 0);
+        } else if ( posX<HALFH ) { // La primera mitad
+            camera.position.set(HALFW, HALFH,0);
+        }
+
+        camera.update();
+    }
+
+    private class Input implements InputProcessor{
+
+        private Vector3 v = new Vector3();
+
+        @Override
+        public boolean keyDown(int keycode) {
+            return false;
+        }
+
+        @Override
+        public boolean keyUp(int keycode) {
+            return false;
+        }
+
+        @Override
+        public boolean keyTyped(char character) {
+            return false;
+        }
+
+        @Override
+        public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+            v.set(screenX,screenY,0);
+            camera.unproject(v);
+
+            if(sophie.getMovementState()==Sophie.MovementState.STILL_LEFT||sophie.getMovementState()==Sophie.MovementState.STILL_RIGHT) {
+                if(camera.position.x - v.x < -522 && v.y < 135){
+                    state = GameState.PAUSED;
+                }else{
+                    //if(!dialogueOn){
+                        if (v.x >= camera.position.x) {
+                            sophie.setMovementState(Sophie.MovementState.MOVE_RIGHT);
+
+                        } else {
+                            sophie.setMovementState(Sophie.MovementState.MOVE_LEFT);
+                        }
+                    //}
+
+                }
+            }
+            return true;
+        }
+
+        @Override
+        public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+            if(sophie.getMovementState()==Sophie.MovementState.MOVE_LEFT)
+                sophie.setMovementState(Sophie.MovementState.STILL_LEFT);
+            else if(sophie.getMovementState() == Sophie.MovementState.MOVE_RIGHT)
+                sophie.setMovementState(Sophie.MovementState.STILL_RIGHT);
+            return true;
+        }
+
+        @Override
+        public boolean touchDragged(int screenX, int screenY, int pointer) {
+            return false;
+        }
+
+        @Override
+        public boolean mouseMoved(int screenX, int screenY) {
+            return false;
+        }
+
+        @Override
+        public boolean scrolled(int amount) {
+            return false;
+        }
     }
 }
