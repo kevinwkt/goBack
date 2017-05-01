552e4076776fd391d9efbaf54c76a621381d9515
Pablo Sanchez <pabloastadeo@gmail.com>
2017-04-30 23:21:38 -0500

    level 2 merge solved

--- core/src/com/tec/goback/Level2.java
+++ core/src/com/tec/goback/Level2.java
@@ -210,14 +210,9 @@ class Level2 extends Frame {
 
 
         }
-<<<<<<< HEAD
+
         Gdx.app.log("time for meteor", timeForMeteor+" - "+squirts.size);
         squirts.clear();
-
-
-
-=======
->>>>>>> 3de1dd7f8e01fc8a23a29a25b6958f43b37981ca
     }
 
 
