d6cb4de3b3956b0110d9fec9870c5dde99765731
Gerry <gerrygoo@gmail.com>
2017-04-30 23:11:04 -0500

    app

--- core/src/com/tec/goback/ArcadeEyes.java
+++ core/src/com/tec/goback/ArcadeEyes.java
@@ -14,107 +14,85 @@ import com.badlogic.gdx.physics.box2d.World;
  * Created by gerry on 4/30/17.
  */
 
-class ArcadeEyes implements IArcadeBoss {
-    private int color;
-    private boolean moving = false;
-    private float life = ArcadeValues.yellowBossLife;
-    private float ac;
-    private Body body;
-    private Sprite sprite;
-    private BodyEditorLoader loader = new BodyEditorLoader(Gdx.files.internal("Physicshit.json"));
+class ArcadeEyes implements IArcadeBoss{
+    private World world;
 
-    ArcadeEyes(World world, int color, Texture tx) {
-        sprite = new Sprite(tx);
-        sprite.setCenter(
-                ArcadeValues.pelletOriginX,
-                ArcadeValues.pelletOriginY
-        );
-        this.color = color;
+    private Eyes[] eyes = new Eyes[3];
 
-        BodyDef bodyDef = new BodyDef();
-        bodyDef.type = BodyDef.BodyType.DynamicBody;
-        bodyDef.position.set(
-                ArcadeValues.pxToMeters(1280+125),
-                ArcadeValues.meterspelletOriginY
-        );
-
-        body = world.createBody(bodyDef);
-        fixturer(0.0f, 0.0f);
-
-        //float SPEED = 4;
-        body.setLinearVelocity(
-                0,
-                0
-        );
-        body.setUserData(this);
-    }
-
-    private void fixturer(float density, float restitution) {
-        FixtureDef fixtureDef = new FixtureDef();
-        fixtureDef.density = density;
-        fixtureDef.restitution = restitution;
-        fixtureDef.friction = 0;
-
-        fixtureDef.filter.categoryBits = ArcadeValues.enemyCat; //its category
-        fixtureDef.filter.maskBits = ArcadeValues.enemyMask; //or of its category with colliding categories
-
-        if(color == 1) {
-            loader.attachFixture(body, "bossLizzard", fixtureDef, 7f);
-        }else{
-            loader.attachFixture(body, "bossJaguar", fixtureDef, 8f);
-        }
+    ArcadeEyes(World world, Texture yellowTx, Texture blueTx, Texture redTx) {
+        eyes[0] = new Eyes(world, 1, new Sprite(yellowTx));
+        eyes[1] = new Eyes(world, 2, new Sprite(blueTx));
+        eyes[2] = new Eyes(world, 3, new Sprite(redTx));
     }
 
     public void move(){
-        if (body.getPosition().x > ArcadeValues.pxToMeters((ArcadeValues.WIDTH + 120))){
-            body.setLinearVelocity(-1f, 0f);
-        }else{
-            body.setLinearVelocity(1f, 0f);
-        }
-        moving = true;
+        //opacity them and set active
     }
 
     public boolean getHurtDie(int color, float damage){
-        life -= color != this.color ? damage : (damage * 2);
-        return life <= 0.0f;
+        for(Eyes e: eyes){
+            if(!e.getHurtDie(1,0)) return false;
+        }
+        return true;
     }
 
     public void draw(SpriteBatch batch) {
-        if(moving && ((body.getPosition().x > ArcadeValues.pxToMeters(ArcadeValues.WIDTH+125) && body.getLinearVelocity().x > 0) || body.getPosition().x < ArcadeValues.pxToMeters(ArcadeValues.bossX) && body.getLinearVelocity().x < 0)){
-            body.setLinearVelocity(0f, 0f);
-            moving = false;
+        for(Eyes e: eyes){
+            e.draw(batch);
         }
-
-        sprite.setPosition(
-                ArcadeValues.metersToPx(body.getPosition().x),
-                ArcadeValues.metersToPx(body.getPosition().y)
-        );
-
-        switch(((int)((life*10)/ArcadeValues.yellowBossLife))){
-            case 10:case 9:
-                sprite.setColor(1f, 1f, 1f, 1f);
-                break;
-            case 8:case 7:  //low opacity
-                sprite.setColor(1f, 1f, 1f, 0.8f);
-                break;
-            case 6:case 5:  //lower opacity
-                sprite.setColor(1f, 1f, 1f, 0.6f);
-                break;
-            case 4:case 3:  //low freq blink
-                blink(0.5f);
-                break;
-            case 2:case 1:  //hi freq blink
-                blink(0.18f);
-                break;
-        }
-
-        sprite.draw(batch);
     }
 
-    private void blink(float speed) {
-        ac = ac <= 1.0 ? ac + Gdx.graphics.getDeltaTime() : 0;
-        sprite.setColor(1f, 1f, 1f, (0.3f * MathUtils.sin(ac * (2 * MathUtils.PI / speed)) + 0.5f));
-    }
 
-    int getColor(){return color;}
+    private class Eyes implements IArcadeBoss{
+        private int color;
+        private World world;
+        private Sprite sprite;
+
+        private float life = ArcadeValues.redBossLife;
+        public Body body;
+
+
+        private BodyEditorLoader loader = new BodyEditorLoader(Gdx.files.internal("Physicshit.json"));
+        Eyes(World world, int color, Sprite sprite){
+            this.world = world;
+            this.color = color;
+            this.sprite = sprite;
+
+            BodyDef bodyDef = new BodyDef();  bodyDef.type = BodyDef.BodyType.DynamicBody;
+            bodyDef.position.set(
+                (color == 1 || color == 2 ? (color == 1 ? ArcadeValues.meterspelletOriginX - 1 :  ArcadeValues.meterspelletOriginX + 1 ) : ArcadeValues.meterspelletOriginX + 2),
+                    ArcadeValues.meterspelletOriginY + 2
+            );
+
+            body = world.createBody(bodyDef);
+            body.setLinearVelocity(1f, 0f);
+
+            FixtureDef fixtureDef = new FixtureDef();
+            fixtureDef.density = 0f;
+            fixtureDef.restitution = 0f;
+            fixtureDef.friction = 0f;
+            fixtureDef.filter.categoryBits = ArcadeValues.enemyCat;
+            fixtureDef.filter.maskBits = ArcadeValues.enemyMask;
+
+            loader.attachFixture(body, "bossEyes", fixtureDef, 1f);
+        }
+
+        public boolean getHurtDie(int color, float damage) {
+            life -= color != this.color ? damage : (damage * 2);
+            return life <= 0.0f;
+        }
+
+        public void move() {
+            if(body.getPosition().x < 0 || body.getPosition().x > ArcadeValues.pxToMeters(1280)) body.setLinearVelocity(-1*body.getLinearVelocity().x, body.getLinearVelocity().y);
+        }
+
+        public void draw(SpriteBatch batch) {
+            move();
+            sprite.setPosition(
+                    ArcadeValues.metersToPx(body.getPosition().x),
+                    ArcadeValues.metersToPx(body.getPosition().y)
+            );
+            sprite.draw(batch);
+        }
+    }
 }
