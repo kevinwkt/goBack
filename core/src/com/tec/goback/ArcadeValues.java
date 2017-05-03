ebc7a2ffb4e6a40790fb14ff267e4cd01bf43b61
Gerry <gerrygoo@gmail.com>
2017-05-03 14:26:32 -0500

    arcade

--- core/src/com/tec/goback/ArcadeValues.java
+++ core/src/com/tec/goback/ArcadeValues.java
@@ -30,9 +30,9 @@ class ArcadeValues {
     //positioning
     static float highOnPot = (float) java.lang.Math.sqrt(640F * 640F + 448F * 448F);
     static final float bossX = 1000;
-    static final float yellowBossLife = 10; //5000;
-    static final float blueBossLife = 10;   //7000;
-    static final float redBossLife = 10;    //9000;
+    static final float yellowBossLife = 5000;
+    static final float blueBossLife = 7000;
+    static final float redBossLife = 9000;
 
     static final float pelletOriginX = 640;
     static final float pelletOriginY = 272;
